02e89b08ee15ecf7f60109b5dea40a7f89567f76
Jimmy <JimmyMall@live.com>
2016-01-04 21:01:49 +0800

    (0.3.2.1) modify css style of go top/bottom button

--- View/Index/footer.php
+++ View/Index/footer.php
@@ -1,5 +1,5 @@
 
-<div class='footer'>
+<div class='footer'><a name='footer'></a>
 	<div class=wrap>
 		&copy;2015 xxx All Rights Reserved. | 
 		<a href='index.php?k=PHP'>联系我</a> | 
@@ -7,10 +7,15 @@
 		<a target='_blank' href='http://www.ruanyifeng.com/' title='Alipay.com as an Node/JavaScript engineer.'>阮一峰</a> | 
 		<a target='_blank' href='http://www.liaoxuefeng.com/'>廖雪峰</a> | 
 		<a target='_blank' href='http://www.zhangxinxu.com/' title='腾讯ISUX。欢迎与我一同交流web前端方面的东西！'>张鑫旭</a> | 
+		<a target='_blank' href='http://www.w3school.com.cn/' title=''>w3school</a> | 
+		<a target='_blank' href='http://www.runoob.com/' title=''>菜鸟教程网</a> | 
 	</div>
 </div>
 
-<a href="#" id="to_top" class="foot_top">返回顶部</a>
+<div class="foot_top">
+	<a href="#" id="to_top">返回顶部</a>
+	<a href="#footer" id="to_top">下到底部</a>
+</div>
 
 </body>
 </html>
