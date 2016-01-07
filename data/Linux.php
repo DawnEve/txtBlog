d2138c3b7916debe723d7888ec0225daf8ee613e
Jimmy <JimmyMall@live.com>
2016-01-07 15:45:39 +0800

    some linux and smarty txt

---
+++ data/Linux.php
@@ -0,0 +1,59 @@
+<?php
+return array (
+  0 => 
+  array (
+    0 => 1,
+    1 => '前言与资料',
+    2 => array (
+      array('项目描述','Linux001','html',),
+      array('参考资料','PHP002','html'),
+      array('FAQ','PHP20160105','html'),
+    ),
+  ),
+  
+  1 => 
+  array (
+    0 => 2,
+    1 => 'Linux常用操作',
+    2 => 
+    array (
+	  array('十大常用命令','Linux101', 'txt'),
+    ),
+  ),
+  
+  
+  
+    2 => 
+  array (
+    0 => 2,
+    1 => 'Linux网络专题',
+    2 => 
+    array (
+	  array('iptables安全防护','Linux201','txt' ),
+    ),
+  ),
+  
+  
+    3 => 
+  array (
+    0 => 2,
+    1 => 'Android专题',
+    2 => 
+    array (
+	  array('Android开发入门1','Android001','html' ),
+	  array('Android开发入门2','Android002','txt' ),
+	  array('Android模拟器','Android003','html' ),
+    ),
+  ),  
+
+  array (
+    0 => 2,
+    1 => 'apache服务器',
+    2 => 
+    array (
+	  array('htaccess配置规则','apache001','txt' ),
+
+    ),
+  ),
+  
+);
--- data/PHP.php
+++ data/PHP.php
@@ -1,27 +1,112 @@
 <?php
 return array (
-  0 => 
+
   array (
     0 => 1,
     1 => '前言与资料',
     2 => array (
       array('项目描述','PHP001','html',),
       array('参考资料','PHP002','html'),
-      array('FAQ','PHP20160105','html'),
+      array('FAQ','PHP20160105-003','html'),
       array('博客示例','PHP000Example','html'),
     ),
   ),
-  1 => 
+  
+  
+  array (
+    0 => 1,
+    1 => '小专题',
+    2 => array (
+      array('文件操作','PHP101','html',),
+
+    ),
+  ),
+  
+  
+
   array (
     0 => 2,
     1 => 'mysql数据库操作',
     2 => 
     array (
-	  array('数据库连接','20151231165021', ),
+	  array('mysql常用操作','20151231165021', ),
       array('select语句','20151231165022', ),
       array('insert语句','20151231165023', ),
       array('update','20151231165024', ),
       array('delete','20151231165025', ),
     ),
   ),
+  
+
+  array (
+    0 => 2,
+    1 => 'smarty3模板引擎',
+    2 => 
+    array (
+      array('项目描述','smarty001','html',),
+      array('参考资料','smarty002','html'),
+      array('FAQ','smarty003','html'),
+      array('入门介绍','smarty104','txt'),
+    ),
+  ),
+  
+
+  array (
+    0 => 2,
+    1 => 'cakephp',
+    2 => 
+    array (
+      array('项目描述','PHP001','html',),
+      array('参考资料','PHP002','html'),
+      array('FAQ','PHP20160105','html'),
+      array('博客示例','PHP000Example','html'),
+    ),
+  ),
+  
+  
+
+  array (
+    0 => 2,
+    1 => 'CI',
+    2 => 
+    array (
+      array('项目描述','PHP001','html',),
+      array('参考资料','PHP002','html'),
+      array('FAQ','PHP20160105','html'),
+      array('博客示例','PHP000Example','html'),
+    ),
+  ),
+  
+    
+  
+
+  array (
+    0 => 2,
+    1 => 'yii',
+    2 => 
+    array (
+      array('项目描述','PHP001','html',),
+      array('参考资料','PHP002','html'),
+      array('FAQ','PHP20160105','html'),
+      array('博客示例','PHP000Example','html'),
+    ),
+  ),
+  
+      
+  
+
+  array (
+    0 => 2,
+    1 => 'zend',
+    2 => 
+    array (
+      array('项目描述','PHP001','html',),
+      array('参考资料','PHP002','html'),
+      array('FAQ','PHP20160105','html'),
+      array('博客示例','PHP000Example','html'),
+    ),
+  ),
+  
+  
+  
 );
