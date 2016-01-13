e0bc1ff83fd6321254ebb6a827b4d818891e29fc
Jimmy <JimmyMall@live.com>
2016-01-13 15:30:21 +0800

    (0.3.3.8) fix bug: log visitor error on firefox

--- View/Index/header.php
+++ View/Index/header.php
@@ -4,10 +4,13 @@
 	<meta name="viewport" content="width=device-width" />
     <meta name="keywords" content="css,javascript,node,jquery,git,python,java,sql,php,mysql,linux,ios,android,教程,软件,编程,开发,运维,云计算,网络,互联网" />
     <meta name="description" content="研究互联网产品和技术，提供原创中文精品教程" />
+	<link rel="shortcut icon" href="/favicon.ico">
 	
 	<link rel="stylesheet" type="text/css" href="public/css/base.css" media="all">
 	<link rel="stylesheet" type="text/css" href="public/css/main.css" media="all">
 	<title>Dawn's Blog: web技术博客 - Powered by txtBLog</title>
+	<script type="text/javascript" src="/public/js/common.js"></script>
+
 </head>
 
 <body>
