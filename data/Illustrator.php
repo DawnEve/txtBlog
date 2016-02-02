f873f1a00c92d7250ffc3e17509ad684534021e6
Dawn <JimmyMall@live.com>
2016-02-02 18:28:40 +0800

    about illustrator

---
+++ data/Illustrator.php
@@ -0,0 +1,25 @@
+<?php
+return array (
+
+  array (
+    0 => 1,
+    1 => '前言与资料',
+    2 => array (
+      array('项目描述','Illustrator001','html',),
+      array('参考资料','Illustrator002','html'),
+      array('FAQ','Illustrator003','html'),
+    ),
+  ),
+  
+  
+  array (
+    0 => 1,
+    1 => 'AI小专题',
+    2 => array (
+      array('入门篇','Illustrator101','html'),
+      array('路径专题','Illustrator102','txt'),
+      array('海报-插画-UI图标','Illustrator103','txt'),
+    ),
+  ),
+  
+);
