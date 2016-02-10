342e25965ff44209eba7e80e9611d02df07478ac
Dawn <JimmyMall@live.com>
2016-02-10 10:50:26 +0800

    illustrator figure

--- data/Illustrator.php
+++ data/Illustrator.php
@@ -8,6 +8,18 @@ return array (
       array('项目描述','Illustrator001','html',),
       array('参考资料','Illustrator002','html'),
       array('FAQ','Illustrator003','html'),
+      array('行业常识','Illustrator004','html'),
+    ),
+  ),
+  
+  
+  array (
+    0 => 1,
+    1 => 'AI实例入门',
+    2 => array (
+      array('入门实例简介','Illustrator101','html'),
+      array('路径专题','Illustrator102','txt'),
+      array('海报-插画-UI图标','Illustrator103','txt'),
     ),
   ),
   
@@ -16,9 +28,7 @@ return array (
     0 => 1,
     1 => 'AI小专题',
     2 => array (
-      array('入门篇','Illustrator101','html'),
-      array('路径专题','Illustrator102','txt'),
-      array('海报-插画-UI图标','Illustrator103','txt'),
+      array('选择与形状','Illustrator201','html'),
     ),
   ),
   
