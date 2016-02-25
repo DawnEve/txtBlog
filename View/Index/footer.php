e48798ddbf3737a8573b71855ab670efc22c1784
Jimmy <JimmyMall@live.com>
2016-02-25 09:40:23 +0800

    add links at footre

--- View/Index/footer.php
+++ View/Index/footer.php
@@ -1,5 +1,6 @@
 
-<div class='footer'><a name='footer'></a>
+<div class='footer'>
+	<a name='footer'></a>
 	<div class=wrap>
 		<div class='fade'>
 			友情链接:
@@ -9,25 +10,42 @@
 			<a target='_blank' href='http://www.nowamagic.net/' title='其实每个程序员都是魔法师。'>简明现代魔法</a> | 
 			<a target='_blank' href='http://www.ycku.com/course/' title='瓢城Web俱乐部'>李炎恢</a> | 
 			<a target='_blank' href='http://limingyang2012.github.io/blog/' title='前端博客'>李明洋</a> | 
-			
-				<a href="http://www.imooc.com/" target="blank">慕课网</a> | 
+			<a target='_blank' href='http://www.bluesdream.com/blog/' title='前端博客'>BlueDream</a> | 
+			<a target='_blank' href='http://ideazhao.com/' title='生活琐碎、前端杂谈、诗情画意、随心笔记'>灵感的小窝</a> | 
 
+				
+				
+				
+				<a href="http://fequan.com/" target="blank">前端圈</a> | 
 				<a href="http://www.75team.com/" target="blank">齐舞团</a> | 
 				<a href="http://f2e.im/" target="blank">F2E</a> | 
 				<a href="http://www.html-js.com/" target="blank">前端乱炖</a> | 
+				<a href="https://www.shiyanlou.com/" target="blank" title='第一家以实验为核心的IT在线教育平台'>实验楼</a> | 
+				
 				
 				<a href="http://www.w3.org/" target="blank">W3C</a> | 
 				<a target='_blank' href='http://www.w3school.com.cn/' title=''>w3school</a> | 
-				<a href="http://www.w3cplus.com/" target="blank">w3cplus</a> | 
 				<a href="http://www.w3ctech.com/" target="blank">w3ctech</a>  | 
 				<a href="http://www.w3cplus.com/" target="blank">w3cplus</a>  | 
+					
 					
 			<a target='_blank' href='http://www.runoob.com/' title=''>菜鸟教程网</a> | 
 			<a target='_blank' href='https://www.v2ex.com/' title='V2EX 是一个关于分享和探索的地方。创意工作者们的社区'>v2ex</a> | 
 			<a target='_blank' href='http://tool.oschina.net/apidocs' title='oschina编程API手册'>API手册</a> | 
-			<a href="http://stackoverflow.com/" target="blank">Stack Overflow</a>
+			<a href="http://stackoverflow.com/" target="blank">Stack Overflow</a> | 
 			<a target='_blank' href='http://segmentfault.com/' title='中文编程问答'>segmentfault</a> | 
 			<a target='_blank' href='http://www.kancloud.cn/explore' title=''>看云文档</a> | 
+			
+			
+			
+			<br />导航:			
+			<a href="http://f2er.club/" target="blank">醉牛前端导航</a> | 
+			<a href="http://caibaojian.com/links" target="blank">前端网址导航</a> | 
+			
+			<br />视频:		
+			<a href="http://www.imooc.com/" target="blank">慕课网</a> | 
+			<a href="http://www.caopeng.com/" target="blank">曹鹏编程之邦</a> | 
+			<a href="http://ninghao.net/" target="blank" title='React，Bootstrap，Laravel 视频教程'>宁皓网</a> | 
 		</div>
 		
 		<div class='links'>
