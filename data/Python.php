54c8e5e0254e6450e672e2e5bae0451c9c4410e1
Jimmy <jimmymall@live.com>
2019-06-13 20:53:42 +0800

    v6.2.3-3south 新增Linux 网络通信安全gpg加密; Python OOP专题

--- data/Python.php
+++ data/Python.php
@@ -7,7 +7,8 @@ return array (
     2 => array (
       array('项目描述','Python001','html',),
       array('参考资料','Python002','html'),
-      array('FAQ','Python003','html'),
+      //array('FAQ','Python003','html'),
+      array('Python准备','Python100','txt',),
     ),
   ),
 
@@ -15,10 +16,9 @@ return array (
     0 => 2,
     1 => 'Python语法',
     2 => array (
-      array('Python准备','Python100','txt',),
       array('Python快速入门','Python101','txt',),
       array('Python基础详解','Python102','txt',),
-      array('Python片段','Python103-pieces','txt',),
+      array('Python OOP','Python103-OOP','txt',),
       array('Python数据处理包','Python104-packages','txt',),
       array('Python Web','Python105-web','txt',),
       array('Python标准库','Python106-Standard_lib','txt',),
