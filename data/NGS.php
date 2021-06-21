9154aa9b61c765b0a841bfe1befd9da02fcb404f
jimmy <JimmyMall@live.com>
2021-06-21 14:09:26 +0800

    v6.3.4-6@SUSTech APA-seq; ATAC-seq

--- data/NGS.php
+++ data/NGS.php
@@ -25,14 +25,15 @@ return array (
 
   array (
     0 => 3,
-    1 => '数据分析语言',
+    1 => '通用分析',
     2 => array (
       array('测序分析shell语句','shell-in-seq','txt'),
       array('构建分析流程 pipeline','pipeline','txt'),
 
 
       array('circos-Cytoscape','circos-Cytoscape','txt'),
-      array('GO分析及可视化','GO_analysis01','txt'),
+      array('富集分析(GO/KEGG)','GO_analysis01','txt'),
+
       array('GEO and TCGA','GEO_TCGA','txt'),
     ),
   ),  
@@ -41,11 +42,12 @@ return array (
     0 => 4,
     1 => 'RNA分析',
     2 => array (
-      array('文件格式','NGS_format','txt'),
+      array('ngs文件格式及互转','NGS_format','txt'),
       array('db_参考基因组_在线工具','genome401','txt'),
       array('本地生信软件(分析)','localSoft402','txt'),
       array('本地生信软件(可视化)','localSoft402-view','txt'),
       array('microRNA分析','miRNA','txt'),
+      array('APA分析','APA-seq','txt'),
     ),
   ),
 
