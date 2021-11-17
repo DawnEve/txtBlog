9f17a0145f4fc918c4f76a39e9ba648d8b08e0ad
jimmy <JimmyMall@live.com>
2021-11-17 22:09:03 +0800

    v6.4.1-0@SUSTech python/ sklearn 预处理,聚类,优化[2021.11.17, Wdn]

--- data/Python.php
+++ data/Python.php
@@ -36,6 +36,7 @@ return array (
       array('Py与算法 algorithm','Py201_algorithm','txt',),
       array('机器学习概述 ML AI','MachineLearning-01','txt',),
       array('sklearn基础教程','sklearn','txt',), //https://scikit-learn.org/stable/
+      array('TensorFlow.txt','TensorFlow','txt',),
       array('python爬虫','webSpider','txt',),
       array('python财经','stocks','txt',),
     ),
